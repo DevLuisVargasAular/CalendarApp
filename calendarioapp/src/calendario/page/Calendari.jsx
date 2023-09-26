e82b2283ea4deb3ebc61d3929c5477b696a725d9
AularJesus <devljesus.aular@gmail.com>
2023-09-25 21:14:58 -0500

    Rutas y librerias de calendarios

--- calendarioapp/src/calendario/page/Calendari.jsx
+++ calendarioapp/src/calendario/page/Calendari.jsx
@@ -1,8 +1,48 @@
 
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import enUS from 'date-fns/locale/en-US';
+
+import { Navbar } from "../";
+import {addHours, format, parse, startOfWeek, getDay} from 'date-fns';
 
 
+const locales = {
+    'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+})
+const events = [{
+title:'Dia de pago',
+notes:'Hay que hacer mercado',
+start: new Date('2023-09-28'),
+end: addHours(new Date(), 2),
+bgColor :'#fafafa',
+user:{
+_id:'123',
+name:'Fernado'}
+
+
+
+}]
 export const Calendari = () => {
-    return(
-        
-        <div>Calendari</div>)
-    }
+    return (
+        <>
+            <Navbar />
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)'}}
+            />
+
+        </>)
+}
--- calendarioapp/src/router/AppRouter.jsx
+++ calendarioapp/src/router/AppRouter.jsx
@@ -1,5 +1,28 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { LoginPage } from '../auth';
+import { Calendari } from '../calendario'
+
+
+
+
+
+
 export const AppRouter = () => {
-    return(
-        
-        <div>AppRouter</div>)
-    }
+
+
+    const authStatus = 'autheticated';
+    return (
+        <Routes>
+            {
+            (authStatus === 'not-autheticated')
+            ?   <Route path='/auth/*' element={<LoginPage />} />
+            : <Route path='/*' element={<Calendari />} />
+   }
+          
+
+           
+            <Route path='/*' element={<Navigate to='/auth/login'/>}/>
+
+        </Routes>
+    )
+}
